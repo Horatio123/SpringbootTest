f0d4ba71b5306c47bb1487b72059635a390604a7
Horatio <alexanderchen1992@gmail.com>
2020-04-26 23:55:59 +0800

    profile and log

--- src/test/java/com/baimuhu/SpringbootTest/SpringbootTestApplicationTests.java
+++ src/test/java/com/baimuhu/SpringbootTest/SpringbootTestApplicationTests.java
@@ -2,6 +2,8 @@ package com.baimuhu.SpringbootTest;
 
 import com.baimuhu.SpringbootTest.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SpringbootTestApplicationTests {
 	@Autowired
 	Person person;
+	Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Test
 	void contextLoads() {
@@ -17,6 +20,16 @@ class SpringbootTestApplicationTests {
 	@Test
 	public void contextLoad() {
 		System.out.println(person);
+	}
+
+	@Test
+	public void testLog() {
+		//log level from low to high
+		logger.trace("this is trace log");
+		logger.debug("this is debug log");
+		logger.info("this is info log");
+		logger.warn("this is warning log");
+		logger.error("this is error log");
 	}
 
 }
