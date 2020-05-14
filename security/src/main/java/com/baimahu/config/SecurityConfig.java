927fc506554ba6978793f666604587817a66c4ec
Horatio <alexanderchen1992@gmail.com>
2020-05-14 23:24:46 +0800

    security project except html

---
+++ security/src/main/java/com/baimahu/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.baimahu.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("VIP1")
+                .antMatchers("/level2/**").hasRole("VIP2")
+                .antMatchers("/level3/**").hasRole("VIP3");
+        http.formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        auth.inMemoryAuthentication()
+                .withUser("Horatio").password("123456").roles("VIP1", "VIP2", "VIP3")
+                .and()
+                .withUser("junye").password("123456").roles("VIP1")
+                .and()
+                .withUser("zhonglei").password("123456").roles("VIP1", "VIP2", "VIP3");
+    }
+}
