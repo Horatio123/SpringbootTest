323505c5d883bb7e725d648f33c62ecae013b69a
Horatio <alexanderchen1992@gmail.com>
2020-05-17 21:38:58 +0800

    actuator and deploy

---
+++ actuator/src/test/java/com/baimahu/ActuatorApplicationTest.java
@@ -0,0 +1,20 @@
+package com.baimahu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ActuatorApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
