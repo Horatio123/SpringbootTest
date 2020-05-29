7f934515b800ff3a5337ee7fec261ff51a634988
Horatio <alexanderchen1992@gmail.com>
2020-05-29 23:33:05 +0800

    emps

---
+++ web/src/main/java/com/baimahu/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.baimahu.controller;
+
+import com.baimahu.dao.EmployeeDao;
+import com.baimahu.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @GetMapping("/emps")
+    public String list(Model model) {
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("emps", employees);
+        return "emp/list";
+    }
+}
