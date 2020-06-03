8ad9bf1cdc3cad07106194cba5f717706776c327
Horatio <alexanderchen1992@gmail.com>
2020-06-04 00:16:38 +0800

    bar.html and active

--- web/src/main/java/com/baimahu/controller/LoginController.java
+++ web/src/main/java/com/baimahu/controller/LoginController.java
@@ -16,7 +16,7 @@ public class LoginController {
     public String login(@RequestParam("username") String username,
                         @RequestParam("password") String password,
                         Map<String, Object> map, HttpSession session) {
-        if (!StringUtils.isEmpty(username) && "123456".equals(password)) {
+        if (!StringUtils.isEmpty(username) && "123".equals(password)) {
             session.setAttribute("loginUser", username);
             return "redirect:/main.html";
         } else {
