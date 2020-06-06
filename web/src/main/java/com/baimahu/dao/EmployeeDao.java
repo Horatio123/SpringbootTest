7e91c1df4eb6169bbe57edaa855a8c1ffa0b308f
Horatio <alexanderchen1992@gmail.com>
2020-06-06 23:41:19 +0800

    list and button

--- web/src/main/java/com/baimahu/dao/EmployeeDao.java
+++ web/src/main/java/com/baimahu/dao/EmployeeDao.java
@@ -20,8 +20,8 @@ public class EmployeeDao {
 	static{
 		employees = new HashMap<>();
 
-		employees.put(1001, new Employee(1001, "E-AA", "aa@163.com", 1, new Department(101, "D-AA")));
-		employees.put(1002, new Employee(1002, "E-BB", "bb@163.com", 1, new Department(102, "D-BB")));
+		employees.put(1001, new Employee(1001, "junye", "aa@163.com", 1, new Department(101, "zhongdian")));
+		employees.put(1002, new Employee(1002, "leiye", "bb@163.com", 1, new Department(102, "hik")));
 		employees.put(1003, new Employee(1003, "E-CC", "cc@163.com", 0, new Department(103, "D-CC")));
 		employees.put(1004, new Employee(1004, "E-DD", "dd@163.com", 0, new Department(104, "D-DD")));
 		employees.put(1005, new Employee(1005, "E-EE", "ee@163.com", 1, new Department(105, "D-EE")));
