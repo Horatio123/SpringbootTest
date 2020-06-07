83b2b760713d58715bafeb3df62f1662ffb61c57
Horatio <alexanderchen1992@gmail.com>
2020-06-07 23:52:51 +0800

    add emp

--- web/src/main/java/com/baimahu/controller/EmployeeController.java
+++ web/src/main/java/com/baimahu/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.baimahu.controller;
 
+import com.baimahu.dao.DepartmentDao;
 import com.baimahu.dao.EmployeeDao;
+import com.baimahu.entities.Department;
 import com.baimahu.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -15,10 +18,26 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
     @GetMapping("/emps")
     public String list(Model model) {
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps", employees);
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model) {
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+        employeeDao.save(employee);
+        System.out.println("xxx: " + employee);
+        return "redirect:/emps";
     }
 }
