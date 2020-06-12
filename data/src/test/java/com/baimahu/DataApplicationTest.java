9686b429462ddd31d4abe385277026de3c0d6343
Horatio <alexanderchen1992@gmail.com>
2020-06-12 23:56:02 +0800

    create data module

---
+++ data/src/test/java/com/baimahu/DataApplicationTest.java
@@ -0,0 +1,40 @@
+package com.baimahu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataApplicationTest
+{
+    @Autowired
+    DataSource dataSource;
+    /**
+     * Rigorous Test :-)
+     */
+
+    @Test
+    public void getDataSourceInfo() throws SQLException {
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
